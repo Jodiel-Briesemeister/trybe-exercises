1a514db32ff93b24e259b3cbf95fd2db97672c19
Jodiel Briesemeister <jodielll29@gmail.com>
2021-04-30 21:08:01 -0300

    Forms react

---
+++ exercises/bloco_12/componentes-com-estado-eventos-e-formularios-com-react.2/my-form-2.0/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MyForm from './components/myForm'
+
+function App() {
+  return (
+    <div>
+      <MyForm />
+    </div>
+  );
+}
+
+
+export default App;
